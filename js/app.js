8a8a9b2be8e84b668d7392c05f6d2ec20c12dc78
Evan Busse <evanbusse@gmail.com>
2016-10-20 11:45:18 -0600

    Calculator evaluates equations

--- js/app.js
+++ js/app.js
@@ -5,7 +5,42 @@ var buttons = $('.buttons');
 
 buttons.children().on('click', function(event){
   var text = $(this).text();
+
   if(!$(this).hasClass('operator')){
-    alert('number');
+    if(screen.text() === ''){
+      screen.text(text);
+    }else{
+      screen.text(screen.text() + text);
+    }
+  }else{
+    if($(this).hasClass('clear')){
+      screen.text('');
+    }else if($(this).hasClass('equals')){
+      evaluate(screen.text());
+    }else {
+      if(screen.text() !== ''){
+      screen.text(screen.text() + ' ' + text + ' ');
+      }
+    }
   }
 });
+
+function evaluate(equation) {
+  var split = equation.split(' ');
+  equation = replace(split);
+  equation = equation.join('');
+
+  screen.text(eval(equation));
+}
+
+
+function replace (arr) {
+  for(var x = 0; x < arr.length; x++) {
+    if(arr[x] === 'x'){
+      arr[x] = '*';
+    }else if(arr[x] === '÷'){
+      arr[x] = '/';
+    }
+  }
+  return arr;
+}
